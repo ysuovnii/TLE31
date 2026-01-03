f42622ba0482ef2b5b16676e59118f7314848a3d
ysuovnii <ysuovnii@gmail.com>
2026-01-03 13:05:17 +0530

    CMT, final approach

--- 1300/1931D.cpp
+++ 1300/1931D.cpp
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long 
-
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int t;
     cin >> t;
     while(t--) {
-        int n;
-        ll x, y;
+        long long n, x, y;
         cin >> n >> x >> y;
-        vector<ll> a(n);
+        vector<long long> a(n);
         for(int i = 0; i < n; i++) cin >> a[i];
-
-        int bp = 0;
+ 
+        map<pair<long long,long long>, int> freq;
+        long long ans = 0;
+ 
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                if(((a[i] + a[j])% x == 0) && ((a[i] - a[j]) % y == 0)) {
-                    bp++;
-                }
-            }
+            long long rX = ((-a[i] % x) + x) % x; 
+            long long rY = (a[i] % y + y) % y;    
+ 
+            ans += freq[{rX, rY}];
+            long long modX = (a[i]%x + x)%x;
+            long long modY = (a[i]%y + y)%y;
+            freq[{modX, modY}]++;
         }
-
-        cout << bp << "\n";
+ 
+        cout << ans << "\n";
     }
-
-    return 0;
 }
